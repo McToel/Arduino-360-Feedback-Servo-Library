49b3bb594bfc2abbfee773c764838e09b19bdb30
McToel <36071676+McToel@users.noreply.github.com>
2019-05-04 12:53:41 +0200

    Add files via upload

---
+++ FeedServo_Basic.ino
@@ -0,0 +1,26 @@
+#include "FeedServo.h"
+
+#define servo_port 7
+#define servo_feedback_port 6
+
+//Create FeedServo Object
+FeedServo FS(servo_port, servo_feedback_port);
+
+void setup()
+{
+  Serial.begin(9600);
+  FS.init();
+}
+
+double Position;
+
+void loop()
+{
+  //Set the servo to 270°, like you could do with the standart servo library
+  FS.write_pos(270);
+  delay(100);
+
+  //Read the position of the servo
+  Position = FS.read_pos();
+  Serial.println(Position);
+}
