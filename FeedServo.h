4a10ef36148a2a4f00483d697434d886b3d7f28d
McToel <36071676+McToel@users.noreply.github.com>
2019-05-04 13:08:48 +0200

    Added write_p_error function

--- FeedServo.h
+++ FeedServo.h
@@ -8,13 +8,14 @@ class FeedServo : public Servo
   public:
 #define dcMin 2.9
 #define dcMax 91.7
-#define s_stop 94
+#define s_stop 95
+#define Kp 1
 
     FeedServo(byte, byte);
-    FeedServo(double*, double*, double*, byte, byte);
     double read_pos();
     void Compute();
     void write_pos(int);
+    void write_p_error(int);
     void init();
 
   private:
@@ -22,9 +23,9 @@ class FeedServo : public Servo
     double last_pos;             //   To detect mouvements in edge cases (350° to 10°)
     int runs;                    //   To calculate absolute position      -> 350° to 370°
 
-    double* myInput;              // * Pointers to the Input, Output, and Setpoint variables
-    double* myOutput;             //   This creates a hard link between the variables and the
-    double* mySetpoint;           //   PID, freeing the user from having to constantly tell us
+    double myInput;              // * Pointers to the Input, Output, and Setpoint variables
+    double myOutput;             //   This creates a hard link between the variables and the
+    double mySetpoint;           //   PID, freeing the user from having to constantly tell us
     //   what these values are.  with pointers we'll just know.
 
     byte port;                    //   Control port of the servo
